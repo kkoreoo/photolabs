bd4331450daa2490c8375fe3c9a9e8fbe30cbc99
atle <alextle24@gmail.com>
2024-02-21 10:29:03 -0700

    added scss to photolistitems

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,12 +6,16 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   /* Insert React */
-  return(
-    <section>
-      <img src={props.sampleDataForPhotoListItem.imageSource}/>
-      <img src={props.sampleDataForPhotoListItem.profile}/>
-      <h3>{props.sampleDataForPhotoListItem.username}</h3>
-      <h3>{`${props.sampleDataForPhotoListItem.location.city}, ${props.sampleDataForPhotoListItem.location.country}`}</h3>
+  return (
+    <section className="photo-list__item">
+      <img className="photo-list__image" src={props.sampleDataForPhotoListItem.imageSource} />
+      <span className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.sampleDataForPhotoListItem.profile} />
+        <div className="photo-list__user-info">
+          <h3>{props.sampleDataForPhotoListItem.username}</h3>
+          <h3 className="photo-list__user.location">{`${props.sampleDataForPhotoListItem.location.city}, ${props.sampleDataForPhotoListItem.location.country}`}</h3>
+        </div>
+      </span>
     </section>
   );
 };

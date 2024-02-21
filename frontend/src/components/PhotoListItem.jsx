7acd622f88ed396a3f1233b8675de4272e83ffaa
atle <alextle24@gmail.com>
2024-02-21 10:01:01 -0700

    created component for photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return(
+    <section>
+      <img src={props.sampleDataForPhotoListItem.imageSource}/>
+      <img src={props.sampleDataForPhotoListItem.profile}/>
+      <h3>{props.sampleDataForPhotoListItem.username}</h3>
+      <h3>{`${props.sampleDataForPhotoListItem.location.city}, ${props.sampleDataForPhotoListItem.location.country}`}</h3>
+    </section>
+  );
 };
 
 export default PhotoListItem;
